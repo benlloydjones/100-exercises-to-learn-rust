f5584f4cd037f87ef72b7f8dc932c15e141f2e93
Ben Lloyd Jones <ben@lloydjones.email>
2024-06-28 18:21:54 +1000

    06 05 complete

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -11,11 +11,11 @@
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fib_mem: Vec<u32> = vec!(0, 1);
+    while fib_mem.len() < (n + 1) as usize {
+        fib_mem.push(fib_mem[fib_mem.len() - 2] + fib_mem[fib_mem.len() - 1]);
+    }
+    fib_mem[n as usize]
 }
 
 #[cfg(test)]
